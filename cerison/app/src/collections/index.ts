33c08a263886df29d12a626af118f0f8501e2cba
CerisonAutomation <cerisonbrown@gmail.com>
2026-04-01 21:37:18 +0200

    feat(cerison): collections index barrel export

---
+++ cerison/app/src/collections/index.ts
@@ -0,0 +1,6 @@
+// Collections barrel export
+export { Pages } from './Pages'
+export { Properties } from './Properties'
+export { Media } from './Media'
+export { Users } from './Users'
+export { Testimonials } from './Testimonials'
